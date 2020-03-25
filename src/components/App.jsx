69da523e8a3dfb573a8b0edf34a5dd9061ec305b
victor tran <victor.tran@tempestas.io>
2020-03-24 20:33:05 -0400

    rename app for emmet abbreviations

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="pulldown-menu">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
