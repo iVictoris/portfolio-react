a3e4635bad213601604fbb46e4893564b2c25c93
Victor Tran <victor.n.tran@outlook.com>
2020-04-03 01:08:07 -0400

    created Page and Home that uses Page component

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import { Page } from '../';
+import React from "react";
+import { Page } from "../";
+import Image from "../../assets/images/ivana-cajina-asuyh-_ZX54-unsplash.jpg";
+import { Link } from "react-router-dom";
 
-const Home = ({ className = '' }) => {
-    return <Page className="home"></Page>;
+const Home = ({ className = "" }) => {
+    return (
+        <Page className="home">
+            <img src={Image} alt="galaxy background" />
+            <div className="resume overview">
+                <header>
+                    <h3>I'm Victor Tran</h3>
+                </header>
+
+                <section className="resume redirect">
+                    <p>
+                        You can read more <Link to="/about">about me</Link>
+                    </p>
+                </section>
+
+                <section className="resume skills"></section>
+            </div>
+        </Page>
+    );
 };
 
 export default Home;
--- src/components/pages/Page.jsx
+++ src/components/pages/Page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Page = ({ className, children }) => {
     return <div className={`page ${className}`.trim()}>{children}</div>;
